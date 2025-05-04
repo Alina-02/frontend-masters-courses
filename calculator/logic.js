d90fdfd8c5adc68868c740c4a8e667332c898750
Alina-02 <alinamontma@gmail.com>
2025-05-04 21:51:51 +0200

    javascript calculator pt1

--- calculator/logic.js
+++ calculator/logic.js
@@ -1,31 +1,103 @@
-let mySquare = document.querySelector(".red-square");
-mySquare.style.width = "300px";
+let total = 0;
+let buffer = "0";
 
-//get class list
-mySquare.classList;
-//add class
-mySquare.classList.add(".classname");
+let lastOperator = null;
 
-//insert html
-mySquare.innerHTML = <em>lololol</em>;
+let result;
 
-//Listeners
+const calculatorDiv = document.querySelector(".calculator-div");
+const paragraphResult = document.querySelector("p.paragraph-result");
 
-const button = document.querySelector(".event-button");
-button.addEventListener("click", () => {
-  alert("hey there");
+calculatorDiv?.addEventListener("click", (event) => {
+  switch (event.target.value) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+      onClickNumber(event.target.value);
+      break;
+    case "/":
+    case "x":
+    case "-":
+    case "+":
+      onClickOperator(event.target.value);
+      break;
+    case "=":
+      onClickEqual();
+      break;
+    case "C":
+      onClickC();
+      break;
+  }
+
+  rerender();
 });
 
-const input = document.querySelector(".input-to-copy");
-const paragraph = document.querySelector(".p-to-copy-to");
+const onClickNumber = (textNumber) => {
+  if (buffer === "0") {
+    buffer = textNumber;
+  } else {
+    buffer += textNumber;
+  }
+};
 
-input.addEventListener("keydown", () => {
-  paragraph.ineerText = input.value;
-});
+const onClickOperator = (operator) => {
+  if (buffer === "0") {
+    return;
+  }
 
-//change event (focus and unfocus)
-input.addEventListener("change", () => {
-  paragraph.style.backgroundColor = input.value;
-});
+  const int = parseInt(buffer);
+  if (total === 0) {
+    total = int;
+  } else {
+    makeOperation(int);
+  }
 
-Number.parseInt("5");
+  lastOperator = operator;
+
+  buffer = "0";
+};
+
+const onClickEqual = () => {
+  if (lastOperator === null) {
+    return;
+  }
+  console.log("hola");
+  makeOperation(parseInt(buffer));
+  buffer += total;
+  lastOperator = null;
+  total = 0;
+  buffer = "0";
+};
+
+const makeOperation = (int) => {
+  switch (lastOperator) {
+    case "+":
+      total += int;
+      break;
+    case "-":
+      total -= int;
+      break;
+    case "/":
+      total /= int;
+      break;
+    case "*":
+      total *= int;
+      break;
+  }
+};
+
+const onClickC = () => {
+  total = 0;
+  buffer = "0";
+};
+
+function rerender() {
+  paragraphResult.textContent = buffer;
+}
