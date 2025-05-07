c53a918e5ecc4c4eb58a4d7c634f94dc9caf2447
Alina-02 <alinamontma@gmail.com>
2025-05-07 15:00:38 +0200

    javascript pt 3

--- complete-intro-to-web-development-v3/word-masters/logic.js
+++ complete-intro-to-web-development-v3/word-masters/logic.js
@@ -17,18 +17,13 @@ let daily_word = getWordOfTheDay();
 let actual_word = "";
 
 let attempts = 1;
-
-document
-  .querySelector(".tester-input")
-  ?.addEventListener("keydown", (event) => {
-    if (isLetter(event.key)) {
-      event.preventDefault();
-    }
-  });
+let win = false;
 
 document.addEventListener("keydown", (event) => {
-  updateActualWord(event.key);
-  rerender();
+  if (attempts < 6 && !win) {
+    updateActualWord(event.key);
+    rerender();
+  }
 });
 
 const updateActualWord = (letter) => {
@@ -73,27 +68,55 @@ const rerender = () => {
 };
 
 const checkActualWord = () => {
-  isValidWord(JSON.stringify({ word: actual_word })).then((valid) => {
-    if (valid) {
-      daily_word.then((dw) => {
-        for (i = 0; i < 5; i++) {
-          if (dw[i] === actual_word[i]) {
-            updateLetterColor(
-              document.getElementById(`letter-${attempts}${i + 1}`),
-              "Correct"
-            );
-          } else if (dw.includes(actual_word[i])) {
-            updateLetterColor(
-              document.getElementById(`letter-${attempts}${i + 1}`),
-              "Semicorrect"
-            );
+  const waitDiv = document.querySelector(".wait-div");
+  waitDiv.innerHTML = "Waiting for a response...";
+
+  isValidWord(JSON.stringify({ word: actual_word }))
+    .then((valid) => {
+      if (valid) {
+        daily_word.then((dw) => {
+          console.log(dw);
+          const all_correct = checkAllLettersCorrect(dw);
+          if (!all_correct) {
+            for (i = 0; i < 5; i++) {
+              console.log(dw, actual_word);
+              if (dw[i] === actual_word[i]) {
+                updateLetterColor(
+                  document.getElementById(`letter-${attempts}${i + 1}`),
+                  "Correct"
+                );
+              } else if (dw.includes(actual_word[i])) {
+                updateLetterColor(
+                  document.getElementById(`letter-${attempts}${i + 1}`),
+                  "Semicorrect"
+                );
+              } else {
+                updateLetterColor(
+                  document.getElementById(`letter-${attempts}${i + 1}`),
+                  "Incorrect"
+                );
+              }
+            }
+            actual_word = "";
+            attempts += 1;
           }
+        });
+      } else {
+        for (i = 0; i < 5; i++) {
+          document
+            .getElementById(`letter-${attempts}${i + 1}`)
+            .classList.add("word-error-animation");
+          setTimeout(() => {
+            document
+              .getElementById(`letter-${attempts}${i + 1}`)
+              .removeClass("word-error-animation");
+          }, 2000);
         }
-        actual_word = "";
-        attempts += 1;
-      });
-    }
-  });
+      }
+    })
+    .finally(() => {
+      waitDiv.innerHTML = "";
+    });
 };
 
 const isLetter = (letter) => {
@@ -123,4 +146,24 @@ const updateLetterColor = (HTMLElement, state) => {
     HTMLElement.style.backgroundColor = "gray";
     HTMLElement.style.color = "white";
   }
+};
+
+const checkAllLettersCorrect = (daily_word) => {
+  let all_correct = true;
+  for (i = 0; i < 5; i++) {
+    if (daily_word[i] !== actual_word[i]) {
+      all_correct = false;
+    }
+  }
+  if (all_correct) {
+    for (i = 0; i < 5; i++) {
+      updateLetterColor(
+        document.getElementById(`letter-${attempts}${i + 1}`),
+        "Correct"
+      );
+    }
+    win = true;
+    window.alert("YOU WIN");
+  }
+  return all_correct;
 };
