81caa4509dbb6387a36f382a525af1485eb41f80
Alina-02 <alinamontma@gmail.com>
2025-08-01 14:36:52 +0200

    introduction to node

---
+++ beginner-learning-path/introduction-to-node/src/command.js
@@ -0,0 +1,93 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import {
+  newNote,
+  getAllNotes,
+  findNotes,
+  removeNote,
+  removeAllNotes,
+} from "./notes.js";
+import { listNotes } from "./utils.js";
+
+yargs(hideBin(process.argv))
+  .command(
+    "new <note>",
+    "Create a new note",
+    (yargs) => {
+      return yargs.positional("note", {
+        type: "string",
+        description: "The content of the note to create",
+      });
+    },
+    async (argv) => {
+      const tags = argv.tags ? argv.tags.split(",") : [];
+      const note = await newNote(argv.note, tags);
+      console.log("Note added! ", note);
+    }
+  )
+  .option("tags", {
+    alias: "t",
+    type: "string",
+    descirption: "tags to add to the note",
+  })
+  .command(
+    "all",
+    "get all notes",
+    () => {},
+    async (argv) => {
+      const notes = await getAllNotes();
+      console.log(listNotes(notes));
+    }
+  )
+  .command(
+    "find <filter>",
+    "get matching notes",
+    (yargs) => {
+      return yargs.positional("filter", {
+        describe:
+          "The search term to filter notes by, will de applied to content",
+        type: "string",
+      });
+    },
+    async (argv) => {
+      const matches = await findNotes(argv.filter);
+      console.log(listNotes(matches));
+    }
+  )
+  .command(
+    "remove <id>",
+    "remove a note by id",
+    (yargs) => {
+      return yargs.positional("id", {
+        type: "number",
+        description: "The id of the note you want to remove",
+      });
+    },
+    async (argv) => {
+      const id = await removeNote(argv.id);
+      console.log(id);
+    }
+  )
+  .command(
+    "webn [port]",
+    "launch website to see notes",
+    (yargs) => {
+      return yargs.positional("port", {
+        describe: "port to bind on",
+        default: 5000,
+        type: "number",
+      });
+    },
+    async (argv) => {}
+  )
+  .command(
+    "clean",
+    "remove all notes",
+    () => {},
+    async (argv) => {
+      await removeAllNotes();
+      console.log("DB reset");
+    }
+  )
+  .demandCommand(1)
+  .parse();
